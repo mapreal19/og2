d6e00ea7da0f0d8be162661fa38e34cc9eb5c85a
Mario Perez <mapreal19@gmail.com>
2022-07-23 09:28:57 +0200

    feat: devise setup

---
+++ spec/requests/application_request_spec.rb
@@ -0,0 +1,11 @@
+require "rails_helper"
+
+RSpec.describe "Application", type: :request do
+  describe "GET /" do
+    it "works" do
+      get "/"
+
+      expect(response).to have_http_status(:ok)
+    end
+  end
+end
